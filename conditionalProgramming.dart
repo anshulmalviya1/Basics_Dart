5fca199f04c69df95b303b1661eca55ad0054efa
Anshul Malviya <88474736+anshulmalviya1@users.noreply.github.com>
2023-08-21 20:43:53 +0530

    commit

---
+++ conditionalProgramming.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  /* 1. Syntax :
+
+  if(condition){
+    true
+  } else{
+    false
+  }  */
+
+  /* 2. Syntax :
+
+  if(condition){
+    true
+  }
+  else if(condition){
+    true
+  } else{
+    false
+  } */
+
+}
---
+++ loops.dart
@@ -0,0 +1,22 @@
+void main(){
+
+  String name = "Anshul";
+
+  // For loop
+  for(int i = 0 ; i < 10 ; i++){
+    print(name);
+  }
+
+  // Do While Loop
+  int n = 10;
+  do{
+    print("No is $n");
+  } while(n < 10);
+
+  // While Loop
+  while(n < 20){
+    print("No is $n");
+    n++;
+  }
+
+}
